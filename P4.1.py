f9744ae4dea51d2c5c2794d24eb43e097db4ab91
chaemni0706 <kcm070611@naver.com>
2023-04-01 22:18:20 +0900

    second

--- P4.1.py
+++ P4.1.py
@@ -1,25 +1,38 @@
-top=[]
+class Stack:
+    def __init__(self):
+        self.top=[]
 
-def isEmpty():
-    return len(top)==0
+    def isEmpty(self): return len(self.top)==0
+    def size(self): return len(self.top)
+    def clear(self): self.top=[]
 
-def push(item):
-    top.append(item)
+    def push(self, item):
+        self.top.append(item)
 
-def pop():
-    if not isEmpty():
-        return top.pop(-1)
+    def pop(self):
+        if not self.isEmpty():
+            return self.top.pop(-1)
+        
+    def peek(self):
+        if not self.isEmpty():
+            return self.top[-1]
     
-def peek():
-    if not isEmpty():
-        return top[-1]
-    
-def size(): return len(top)
-def clear():
-    global top
-    top=[]
+    def __str__(self):
+        return str(self.top[::-1])
 
-for i in range(1, 6):
-    push(i)
-print(' push 5회: ', top)
-print('')
+odd=Stack()
+even=Stack()
+
+for i in range(10):
+    if i%2==0: even.push(i)
+    else: odd.push(i)
+
+print(' 스택 even push 5회: ', even.top)
+print(' 스택 odd push 5회: ', odd.top)
+print(' 스택 even peek: ', even.peek())
+print(' 스택 odd peek: ', odd.peek())
+
+for _ in range(2): even.pop()
+for _ in range(3): odd.pop()
+print(' 스택 even pop 2회: ', even.top)
+print(' 스택 odd pop 3회: ', odd.top)
